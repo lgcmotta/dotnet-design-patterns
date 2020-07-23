ffe05201b21e0e89401c9f75b75a98e5991f56ac
Luiz Motta <luizmotta01@hotmail.com>
2020-07-22 23:38:04 -0300

    feat: add faceted builder

---
+++ src/DesignPatterns/Builder/Faceted/PersonBuilder.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.Builder.Faceted
+{
+    public class PersonBuilder
+    {
+        protected Person Person = new Person();
+
+        public PersonJobBuilder Works => new PersonJobBuilder(Person);
+
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(Person);
+
+        public Person Build()
+        {
+            return Person;
+        }
+    }
+}
