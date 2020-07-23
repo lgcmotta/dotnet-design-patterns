73ce7f3ca86c6cb874997801a3c54272db1aa766
Luiz Motta <luizmotta01@hotmail.com>
2020-07-22 23:37:42 -0300

    feat: add fluent builder

---
+++ src/DesignPatterns.Tests/Builder/Fluent/FluentTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DesignPatterns.Builder.Fluent;
+using NUnit.Framework;
+
+namespace DesignPatterns.Tests.Builder.Fluent
+{
+    public class FluentTests
+    {
+        private string _expectedHtml;
+
+        [SetUp]
+        public void Setup()
+        {
+            _expectedHtml = new StringBuilder()
+                .Append("<!DOCTYPE html>\n")
+                .Append("<html>\n")
+                .Append("  <head>\n")
+                .Append("    <title>My Home Page</title>\n")
+                .Append("  </head>\n")
+                .Append("  <body>\n")
+                .Append("    <h1 style=\"font-weight:300\">Heading 1</h1>\n")
+                .Append("    <h2 style=\"font-weight:300\">Heading 2</h2>\n")
+                .Append("  </body>\n")
+                .Append("</html>\n\n")
+                .ToString();
+        }
+
+        [Test]
+        public void CreateHtmlDocument()
+        {
+            var htmlDocument = new HtmlBuilder()
+                .AppendHeadChild("title", "My Home Page")
+                .AppendBodyChild("h1", "Heading 1", new[] {("style", "font-weight:300")})
+                .AppendBodyChild("h2", "Heading 2", new[] {("style", "font-weight:300")})
+                .Build();
+
+            Assert.AreEqual(_expectedHtml.Trim(),htmlDocument.Trim());
+
+        }
+    }
+}
---
+++ src/DesignPatterns/Builder/Fluent/HtmlAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPatterns.Builder.Fluent
+{
+    public class HtmlAttribute
+    {
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return string.IsNullOrEmpty(Key) || string.IsNullOrEmpty(Value) ? string.Empty : $"{Key}=\"{Value}\"";
+        }
+    }
+}
