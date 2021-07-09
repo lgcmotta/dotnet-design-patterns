6b179b1c37e85a04270073674c4e5235e004927f
luizmotta01 <luizmotta01@hotmail.com>
2021-07-09 19:12:08 -0300

    test: builder pattern

---
+++ tests/DesignPatterns.Tests/Builders/ShoppingListBuilderTests.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections.Generic;
+using DesignPatterns.Builders;
+using FluentAssertions;
+using Xunit;
+
+namespace DesignPatterns.Tests.Builders
+{
+    public class ShoppingListBuilderTests
+    {
+        [Fact]
+        public void CreatingInstanceShoppingListBuilder_DefaultConstructor_Success()
+        {
+            using var shoppingListBuilder = new ShoppingListBuilder();
+
+            shoppingListBuilder.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void CreatingInstanceShoppingListBuilder_NonDefaultConstructor_Success()
+        {
+            using var shoppingListBuilder = new ShoppingListBuilder(new ShoppingListSectionBuilder());
+
+            shoppingListBuilder.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void EmptyShoppingListCreated_Success()
+        {
+            using var shoppingListBuilder = new ShoppingListBuilder();
+
+            var shoppingList = shoppingListBuilder.Build();
+
+            shoppingList.Should().NotBeNull();
+            shoppingList.Title.Should().BeNullOrEmpty();
+            shoppingList.Sections.Should().BeNullOrEmpty();
+        }
+        
+        [Fact]
+        public void AddingTitleToShoppingList_Success()
+        {
+            using var shoppingListBuilder = new ShoppingListBuilder();
+
+            var shoppingList = shoppingListBuilder.WithTitle("Monthly shopping").Build();
+
+            shoppingList.Title
+                .Should()
+                .NotBeNullOrEmpty()
+                .And
+                .BeEquivalentTo("Monthly shopping");
+        }
+
+        [Fact]
+        public void AddingSingleSectionToShoppingList_Success()
+        {
+            using var shoppingListBuilder = new ShoppingListBuilder();
+
+            var shoppingList = shoppingListBuilder
+                .AppendGroup(builder => builder.WithDescription("Laundry items"))
+                .Build();
+
+            shoppingList.Sections
+                .Should()
+                .NotBeNullOrEmpty()
+                .And
+                .HaveCount(1)
+                .And
+                .ContainSingle(section => section.Description == "Laundry items");
+        }
+
+        [Fact]
+        public void AddingMultipleSectionsToShoppingList_Success()
+        {
+            using var shoppingListBuilder = new ShoppingListBuilder();
+
+            var shoppingList = shoppingListBuilder
+                .AppendGroup(builder => builder.WithDescription("Laundry items"))
+                .AppendGroup(builder => builder.WithDescription("Food"))
+                .AppendGroup(builder => builder.WithDescription("Bathroom items"))
+                .Build();
+
+            shoppingList.Sections.Should()
+                .NotBeNullOrEmpty()
+                .And
+                .HaveCount(3)
+                .And
+                .BeEquivalentTo(expectations:
+                    new object[]
+                    {
+                        new Section() {Description = "Laundry items"},
+                        new Section() {Description = "Food"},
+                        new Section() {Description = "Bathroom items"}
+                    });
+        }
+
+        [Fact]
+        public void CreatingMultipleInstancesOfShoppingList_NotTheSameInstance_Success()
+        {
+            using var shoppingListBuilder = new ShoppingListBuilder();
+
+            var shoppingList1 = shoppingListBuilder.Build();
+            
+            var shoppingList2 = shoppingListBuilder.Build();
+
+            shoppingList1.Should().NotBeSameAs(shoppingList2);  
+        }
+
+        [Fact]
+        public void CreatingMultipleShoppingLists_WithoutCleaningTheBuilder_Success()
+        {
+            using var shoppingListBuilder = new ShoppingListBuilder();
+
+            var shoppingList1 = shoppingListBuilder.WithTitle("Shopping List 1").Build();
+
+            var shoppingList2 = shoppingListBuilder.Build();
+
+            shoppingList1.Title.Should().BeSameAs(shoppingList2.Title);
+        }
+
+        [Fact]
+        public void CreatingMultipleShoppingLists_CleaningTheBuilder_Success()
+        {
+            using var shoppingListBuilder = new ShoppingListBuilder();
+
+            var shoppingList1 = shoppingListBuilder.WithTitle("Shopping List 1").Build();
+
+            shoppingListBuilder.Clear();
+
+            var shoppingList2 = shoppingListBuilder.Build();
+
+            shoppingList1.Title.Should().NotBeSameAs(shoppingList2.Title);
+        }
+
+        [Fact]
+        public void CreatingMultipleShoppingList_OverridingLastCall_Success()
+        {
+            using var shoppingListBuilder = new ShoppingListBuilder();
+
+            var shoppingList1 = shoppingListBuilder.WithTitle("Shopping List 1").Build();
+
+            var shoppingList2 = shoppingListBuilder.WithTitle("Shopping List 2").Build();
+
+            shoppingList1.Title.Should().NotBeSameAs(shoppingList2.Title);
+
+            Assert.Equal("Shopping List 1", shoppingList1.Title);
+            
+            Assert.Equal("Shopping List 2", shoppingList2.Title);
+        }
+    }
+}
---
+++ tests/DesignPatterns.Tests/Builders/ShoppingListSectionBuilderTests.cs
@@ -0,0 +1,73 @@
+﻿using DesignPatterns.Builders;
+using FluentAssertions;
+using Xunit;
+
+namespace DesignPatterns.Tests.Builders
+{
+    public class ShoppingListSectionBuilderTests
+    {
+        [Fact]
+        public void CreatingInstanceOfShoppingListSectionBuilder_Success()
+        {
+            using var sectionBuilder = new ShoppingListSectionBuilder();
+
+            sectionBuilder.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void EmptyShoppingListSection_Success()
+        {
+            using var sectionBuilder = new ShoppingListSectionBuilder();
+
+            var section = sectionBuilder.Build();
+
+            section.Should().NotBeNull();
+            section.Description.Should().BeNullOrEmpty();
+            section.Items.Should().BeNullOrEmpty();
+        }
+
+        [Fact]
+        public void AddingDescriptionToShoppingListSection_Success()
+        {
+            using var sectionBuilder = new ShoppingListSectionBuilder();
+
+            var section = sectionBuilder.WithDescription("Food").Build();
+
+            Assert.Equal("Food", section.Description);
+        }
+
+        [Fact]
+        public void AddingMultipleItemsToSection_Success()
+        {
+            using var sectionBuilder = new ShoppingListSectionBuilder();
+
+            var section = sectionBuilder.WithDescription("Snacks")
+                .AppendItem("Popcorn", 1)
+                .AppendItem("Cheetos", 2)
+                .AppendItem("Cookies pack", 4)
+                .Build();
+
+            section.Items.Should()
+                .NotBeNullOrEmpty()
+                .And
+                .HaveCount(3);
+        }
+
+        [Fact]
+        public void AddingSameItemTwice_AddsAmount_Success()
+        {
+            using var sectionBuilder = new ShoppingListSectionBuilder();
+
+            var section = sectionBuilder.WithDescription("Snacks")
+                .AppendItem("Popcorn", 1)
+                .AppendItem("Popcorn", 6)
+                .AppendItem("Cookies pack", 4)
+                .Build();
+            
+            section.Items.Should()
+                .HaveCount(2)
+                .And
+                .ContainSingle(item => item.Name == "Popcorn" && item.Amount == 7);
+        }
+    }
+}
