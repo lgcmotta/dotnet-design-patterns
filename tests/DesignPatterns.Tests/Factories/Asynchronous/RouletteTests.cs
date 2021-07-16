497a2d20e0bb45185339f65ff06d7abfdc16c2e6
luizmotta01 <luizmotta01@hotmail.com>
2021-07-16 00:04:14 -0300

    refactor: change async factory to roulette wheel context

---
+++ tests/DesignPatterns.Tests/Factories/Asynchronous/RouletteTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using System.Threading.Tasks;
+using DesignPatterns.Factories.Asynchronous;
+using FluentAssertions;
+using Xunit;
+
+namespace DesignPatterns.Tests.Factories.Asynchronous
+{
+    public class RouletteTests
+    {
+        [Fact]
+        public async Task TurnTheRoulette_Success()
+        {
+            var (color, number) = await Roulette.Turn();
+
+            var possibleColors = new[] { Color.Green, Color.Black, Color.Red };
+
+            possibleColors.Should().Contain(color);
+                
+            number.Should().BeGreaterOrEqualTo(0).And.BeLessOrEqualTo(32);
+        }
+    }
+}
