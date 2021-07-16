221983997eb9563c4f5c170c0a2d1361cb83b743
luizmotta01 <luizmotta01@hotmail.com>
2021-07-16 01:26:47 -0300

    refactor: change context of cloneable prototype sample

--- src/DesignPatterns/Prototypes/Cloneable/Line.cs
+++ src/DesignPatterns/Prototypes/Cloneable/Line.cs
@@ -24,27 +24,4 @@ namespace DesignPatterns.Prototypes.Cloneable
             return new(Start.DeepClone(), End.DeepClone());
         }
     }
-
-    public class Point : ICloneable, IDeepCloneable<Point>
-    {
-        public int X { get; set; }
-
-        public int Y { get; set; }
-
-        public Point(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        public object Clone()
-        {
-            return new Point(X, Y);
-        }
-
-        public Point DeepClone()
-        {
-            return new (X, Y);
-        }
-    }
 }
