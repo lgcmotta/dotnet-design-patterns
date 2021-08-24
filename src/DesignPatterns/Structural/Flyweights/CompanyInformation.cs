2359b78054c780c290c8d9a360c5c1d86d8832f1
luizmotta01 <luizmotta01@hotmail.com>
2021-08-24 15:32:04 -0300

    feat: flyweight + tests

---
+++ src/DesignPatterns/Structural/Flyweights/CompanyInformation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DesignPatterns.Structural.Flyweights
+{
+    public class CompanyInformation
+    {
+        public string Name { get; set; }
+
+        public Uri WebSite { get; set; }
+    }
+
+    public static class CompanyInformationFactory
+    {
+        public static readonly CompanyInformation Company = new()
+        {
+            Name = "Motta Inc.",
+            WebSite = new Uri("https://mottainc.com")
+        };
+    }
+}
---
+++ tests/DesignPatterns.Tests/Structural/Flyweights/CompanyInformationTests.cs
@@ -0,0 +1,20 @@
+﻿using DesignPatterns.Structural.Flyweights;
+using FluentAssertions;
+using Xunit;
+
+namespace DesignPatterns.Tests.Structural.Flyweights
+{
+    public class CompanyInformationTests
+    {
+        [Fact]
+        public void CompanyInformationShouldNotHaveMultipleAllocations()
+        {
+            var companyInfo1 = CompanyInformationFactory.Company;
+            var companyInfo2 = CompanyInformationFactory.Company;
+
+            companyInfo1.Should().BeSameAs(companyInfo2);
+            companyInfo1.WebSite.Should().BeSameAs(companyInfo2.WebSite);
+            companyInfo1.Name.Should().BeSameAs(companyInfo2.Name);
+        }
+    }
+}
